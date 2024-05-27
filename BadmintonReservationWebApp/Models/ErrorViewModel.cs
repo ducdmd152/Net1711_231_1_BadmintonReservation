ab1999ee0969a70b226ab09b087421799160a0cb
vanthong <vanthong07012002@gmail.com>
2024-05-27 15:51:40 +0700

    Fix: Project razor page -> MVC

---
+++ BadmintonReservationWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BadmintonReservationWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
