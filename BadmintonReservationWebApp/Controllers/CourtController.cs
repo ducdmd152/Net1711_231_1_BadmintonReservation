34ce157975c32ac5d3ac8c728e5f76cf80a7f9a1
vanthong <vanthong07012002@gmail.com>
2024-05-29 22:50:26 +0700

    Add controller in mvc

---
+++ BadmintonReservationWebApp/Controllers/CourtController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BadmintonReservationData;
+using Newtonsoft.Json;
+
+namespace BadmintonReservationWebApp.Controllers
+{
+    public class CourtController : Controller
+    {
+        private const string URL = "https://localhost:7257/";
+
+        // GET: Court
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(this);
+        }
+
+        [HttpGet]
+        public async Task<List<Court>> GetAll()
+        {
+            try
+            {
+                var result = new List<Court>();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync(URL + "api"))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            result = JsonConvert.DeserializeObject<List<Court>>(content);
+                        }
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
