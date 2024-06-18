ee7271c7169c071b5088f2c97bf68ab017c5bd4a
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-06-18 11:48:07 +0700

    [Booking Page] Done filtering

--- BadmintonReservationData/Repository/BookingRepository.cs
+++ BadmintonReservationData/Repository/BookingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BadmintonReservationData.Base;
+using BadmintonReservationData.DTO;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,28 @@ namespace BadmintonReservationData.Repository
                               .ThenInclude(frame => frame.Court)
                               .ToListAsync();
         }
+
+        public async Task<List<Booking>> GetAllWithFilterWithDetailsAsync(BookingFilterDTO filter)
+        {
+            return await this._dbSet
+                              .IgnoreAutoIncludes()
+                              .Where(item => item.Id.ToString().Contains(filter.SearchText) 
+                                  || item.Customer.FullName.ToLower().Contains(filter.SearchText.ToLower()) 
+                                  || item.Customer.PhoneNumber.ToLower().Contains(filter.SearchText.ToLower()))
+                              .Where(item => filter.Status == 0 || item.Status == filter.Status)
+                              .Where(item => filter.BookingType == 0 || item.BookingTypeId == filter.BookingType)
+                              .Where(item => filter.PaymentType == 0 || item.PaymentType == filter.PaymentType)
+                              .Where(item => filter.PaymentStatus == 0 || item.PaymentStatus == filter.PaymentStatus)
+                              .Where(item => filter.BookingDateFrom == null || item.BookingDetails.Any(detail => detail.BookDate >= filter.BookingDateFrom))
+                              .Where(item => filter.BookingDateTo == null || item.BookingDetails.Any(detail => detail.BookDate <= filter.BookingDateTo))
+                              .Include(item => item.Customer)
+                              .Include(item => item.BookingType)
+                              .Include(item => item.Payment)
+                              .Include(item => item.BookingDetails)
+                              .ThenInclude(bookingDetail => bookingDetail.Frame)
+                              .ThenInclude(frame => frame.Court)
+                              .ToListAsync();
+        }        
 
         public async Task<Booking> GetByIdWithDetailsAsync(int id)
         {
